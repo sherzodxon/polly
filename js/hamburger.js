2246f039ba0148a6c872fea4d1d1f319fb7cd132
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2022-12-24 12:19:13 +0500

    info section adaptive is finished

---
+++ js/hamburger.js
@@ -0,0 +1,23 @@
+const hambButton = document.querySelector("#hamb-button");
+const nav=document.querySelector(".header-nav");
+const shops=document.querySelector(".header-button-wrapper");
+const headerModal=document.querySelector(".header-modal");
+const navModal =document.querySelector(".header-nav-modal");
+
+hambButton.addEventListener("click",function(){
+    hambButton.classList.toggle("header-hamburger-close-button");
+    nav.classList.toggle("header-nav--active");
+    shops.classList.toggle("header-button-wrapper--active");
+    headerModal.classList.toggle("header-modal--active");
+    navModal.classList.toggle("header-nav-modal--active")
+   
+});
+headerModal.addEventListener("click",function (evt) {
+    if(evt.target.matches(".header-modal--active")){
+        headerModal.classList.remove("header-modal--active");
+        hambButton.classList.remove("header-hamburger-close-button");
+        nav.classList.remove("header-nav--active");
+        shops.classList.remove("header-button-wrapper--active");
+        navModal.classList.remove("header-nav-modal--active")
+    }
+})
