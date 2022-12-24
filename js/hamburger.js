ad4944423a13999af40108253de1d19b3d825fc1
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2022-12-24 20:24:00 +0500

    adaptive is finished

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,13 +3,16 @@ const nav=document.querySelector(".header-nav");
 const shops=document.querySelector(".header-button-wrapper");
 const headerModal=document.querySelector(".header-modal");
 const navModal =document.querySelector(".header-nav-modal");
+const hmBody  = document.querySelector("body")
 
 hambButton.addEventListener("click",function(){
     hambButton.classList.toggle("header-hamburger-close-button");
     nav.classList.toggle("header-nav--active");
     shops.classList.toggle("header-button-wrapper--active");
     headerModal.classList.toggle("header-modal--active");
-    navModal.classList.toggle("header-nav-modal--active")
+    navModal.classList.toggle("header-nav-modal--active");
+    hmBody.classList.remove("body")
+    hmBody.classList.toggle("body-hidden");
    
 });
 headerModal.addEventListener("click",function (evt) {
@@ -18,6 +21,7 @@ headerModal.addEventListener("click",function (evt) {
         hambButton.classList.remove("header-hamburger-close-button");
         nav.classList.remove("header-nav--active");
         shops.classList.remove("header-button-wrapper--active");
-        navModal.classList.remove("header-nav-modal--active")
+        navModal.classList.remove("header-nav-modal--active");
+        hmBody.classList.remove("body-hidden")
     }
 })
